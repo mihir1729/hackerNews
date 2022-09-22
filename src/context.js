9829248b1ab56e1e5b3026fcfb51479aae40d7db
Mihir Yadav <72518307+mihir1729@users.noreply.github.com>
2022-09-22 21:36:35 +0530

    first commit

---
+++ src/context.js
@@ -0,0 +1,52 @@
+import React, { useContext, useEffect, useReducer } from "react";
+
+import {
+	SET_LOADING,
+	SET_STORIES,
+	REMOVE_STORY,
+	HANDLE_PAGE,
+	HANDLE_SEARCH,
+} from "./actions";
+import reducer from "./reducer";
+
+const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?";
+
+const initialState = {
+	isLoading: true,
+	hits: [],
+	query: "react",
+	page: 0,
+	nbPages: 0,
+};
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+	const [state, dispatch] = useReducer(reducer, initialState);
+
+	const fetchStories = async (url) => {
+		dispatch({ type: SET_LOADING });
+
+		try {
+			const response = await fetch(url);
+			const data = await response.json();
+			console.log(data);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	useEffect(() => {
+		fetchStories();
+	}, []);
+
+	return (
+		<AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+	);
+};
+// make sure use
+export const useGlobalContext = () => {
+	return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
